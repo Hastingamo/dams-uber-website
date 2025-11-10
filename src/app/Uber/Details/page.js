4731f125469d713d40bb53d8970cc26aaece5514
Hastingamo <hezekiahhastings14@gmail.com>
2025-11-10 18:57:44 +0100

    the

--- src/app/Uber/Details/page.js
+++ src/app/Uber/Details/page.js
@@ -1,7 +1,9 @@
 "use client"
 import React from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
-export default function page() {
+import next from 'next'
+import { useState, useCallback } from 'react'
+export default function Page() {
     const containerStyle = {
   width: '400px',
   height: '400px',
@@ -16,9 +18,9 @@ const center = {
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   })
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = useState(null)
 
-  const onLoad = React.useCallback(function callback(map) {
+  const onLoad = useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center)
     map.fitBounds(bounds)
@@ -26,7 +28,7 @@ const center = {
     setMap(map)
   }, [])
 
-  const onUnmount = React.useCallback(function callback(map) {
+  const onUnmount = useCallback(function callback(map) {
     setMap(null)
   }, [])
 
