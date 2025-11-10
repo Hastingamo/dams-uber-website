2356cd1b9c500e165645ba523202262ad9feefbf
Hastingamo <hezekiahhastings14@gmail.com>
2025-11-10 19:01:05 +0100

    thhh

--- src/app/Uber/page.js
+++ src/app/Uber/page.js
@@ -2,7 +2,7 @@
 import { useRouter } from 'next/navigation';
 import React from 'react'
 
-function page() {
+function Page() {
   const routes = useRouter();
 
   const search = () => {
@@ -23,4 +23,4 @@ function page() {
   )
 }
 
-export default page
+export default Page
