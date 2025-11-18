8b94ccfcb816f5773dc7be1b769585d886300a52
Hastingamo <hezekiahhastings14@gmail.com>
2025-11-18 01:36:17 +0100

    ttttttt

--- src/app/Uber/page.js
+++ src/app/Uber/page.js
@@ -1,26 +1,59 @@
 "use client";
-import { useRouter } from 'next/navigation';
-import React from 'react'
-
+import { useRouter } from "next/navigation";
+import React from "react";
+import Image from "next/image";
 function Page() {
   const routes = useRouter();
 
   const search = () => {
-    routes.push("Uber/Details")
-  } 
+    routes.push("Uber/Details");
+  };
   return (
     <div>
-      <h1>
-        uber  
-    </h1>
-    <input placeholder='where to?' onClick={search} className='border p-1 w-full rounded-2xl'/>
-      <div className='grid grid-cols-3 mt-4'>
-        <button className='border shadow-2xl'>trip</button>
-        <button className='border shadow-2xl'>send item</button>
-        <button className='border shadow-2xl'>reserve</button>
+      <h1>uber</h1>
+      <input
+        placeholder="where to?"
+        onClick={search}
+        className="border p-1 w-full rounded-2xl"
+      />
+      <div className="grid grid-cols-3 md:grid-cols-4 gap-4 mt-4">
+        <button className="border shadow-2xl ml-4 rounded-2xl py-2">
+          {" "}
+          <Image
+            src="/Image/taxis.png"
+            className="ml-10"
+            alt="taxi"
+            width={30}
+            height={20}
+          />
+          <h1>trip</h1>
+        </button>
+        <button className="border shadow-2xl rounded-2xl py-2 ">
+          {" "}
+          <Image
+            src="/Image/delivery-man.png"
+            className="ml-10 md:ml-20"
+            alt="taxi"
+            width={30}
+            height={20}
+          />
+          <h1>send item</h1>
+        </button>
+        <button className="border shadow-2xl rounded-2xl mr-5 py-2 ">
+          {" "}
+          <Image
+            src="/Image/booking.png"
+            className="ml-10"
+            alt="taxi"
+            width={30}
+            height={20}
+          />
+          <h1>reserve</h1>
+        </button>
+        <button className="border shadow-2xl">store pickup</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
